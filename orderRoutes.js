7bc1890fced68ce3eae32686fb63b04111cf60ed
Prins Soni <princesoni5010@gmail.com>
2026-06-27 11:59:16 +0530

    Update Order model import path in orderRoutes.js

--- orderRoutes.js
+++ orderRoutes.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Order = require('../models/Order');
-
+const Order = require('./config/models/Order');
 router.get('/', async (req, res) => {
   try {
     const orders = await Order.find();
