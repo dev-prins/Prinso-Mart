2013903e6d5ddc3e57c4c61302badba37b8825cb
Prins Soni <princesoni5010@gmail.com>
2026-06-27 16:14:34 +0530

    Implement CRUD routes for settings
    Added routes for getting, creating, and updating settings.

--- settingRoutes.js
+++ settingRoutes.js
@@ -1,8 +1,39 @@
 const express = require('express');
 const router = express.Router();
+const Setting = require('./config/models/Setting');
 
-router.get('/', (req, res) => {
-  res.json({ message: 'Settings routes working' });
+// GET all settings
+router.get('/', async (req, res) => {
+  try {
+    const settings = await Setting.findOne();
+    res.json(settings);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+// POST create settings
+router.post('/', async (req, res) => {
+  try {
+    const setting = await Setting.create(req.body);
+    res.status(201).json(setting);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+// PUT update settings
+router.put('/', async (req, res) => {
+  try {
+    const setting = await Setting.findOneAndUpdate(
+      {},
+      req.body,
+      { new: true, upsert: true }
+    );
+    res.json(setting);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 });
 
 module.exports = router;
