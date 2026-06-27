81954790b611cbb723c7cb10732df7f331be74ff
Prins Soni <princesoni5010@gmail.com>
2026-06-27 11:28:14 +0530

    Fix import path for userModel in authRoutes

--- authRoutes.js
+++ authRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const User = require('../models/userModel');
+const User = require('./models/userModel');
 
 router.post('/register', async (req, res) => {
   try {
