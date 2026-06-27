bf579ddfe7d1dd52806478df621b5b56b4280fe9
Prins Soni <princesoni5010@gmail.com>
2026-06-27 12:00:13 +0530

    Update User model import path in userRoutes.js

--- userRoutes.js
+++ userRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/userModel');
+const User = require('./config/models/userModel');
 
 router.get('/', async (req, res) => {
   try {
