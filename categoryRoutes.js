e5e7049193cd728e579449d4281a514bb5060d5b
Prins Soni <princesoni5010@gmail.com>
2026-06-27 11:58:48 +0530

    Update Category model import path

--- categoryRoutes.js
+++ categoryRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Category = require('../models/Category');
+const Category = require('./config/models/Category');
 
 router.get('/', async (req, res) => {
   try {
