e50d52d5ddf45df435fd957109b81c5911e1e643
Prins Soni <princesoni5010@gmail.com>
2026-06-28 17:05:48 +0530

    Update Product.js

--- config/models/Product.js
+++ config/models/Product.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
-  name: { type: String, required: true },
+  nameEn: { type: String, required: true }, // इंग्लिश नाम
+  nameHi: { type: String, required: true }, // हिंदी नाम
   price: { type: Number, required: true },
   originalPrice: { type: Number },
   unit: { type: String, default: '1 kg' },
@@ -9,7 +10,8 @@ const productSchema = new mongoose.Schema({
   stock: { type: Number, default: 0 },
   discount: { type: Number, default: 0 },
   emoji: { type: String, default: '🛒' },
-  image: { type: String, default: '' }
+  image: { type: String, default: '' },
+  isActive: { type: Boolean, default: true } // यह लाइन 'Hide/Show' के लिए है
 }, { timestamps: true });
 
 module.exports = mongoose.model('Product', productSchema);
