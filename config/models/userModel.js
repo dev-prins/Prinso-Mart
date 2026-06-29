bd029ab7bc9d0279820fd7d77163910deb8649e2
Prins Soni <princesoni5010@gmail.com>
2026-06-30 01:40:58 +0530

    Update userModel.js

--- config/models/userModel.js
+++ config/models/userModel.js
@@ -1,31 +1,57 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  phone: { type: String },
+  name: {
+    type: String,
+    required: true,
+  },
+
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+
+  password: {
+    type: String,
+    required: true,
+  },
+
+  phone: {
+    type: String,
+    default: "",
+  },
+
   role: {
     type: String,
-    enum: ['customer', 'admin', 'delivery_partner'],
-    default: 'customer',
+    enum: ["customer", "admin", "delivery_partner"],
+    default: "customer",
   },
-  isApproved: { type: Boolean, default: false },
-  createdAt: { type: Date, default: Date.now },
+
+  isApproved: {
+    type: Boolean,
+    default: false,
+  },
+}, {
+  timestamps: true,
 });
 
-userSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) {
-    next();
+// Hash password before saving
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
   }
+
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
+
+  next();
 });
 
+// Compare password
 userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
 
-const User = mongoose.model('User', userSchema);
-module.exports = User;
+module.exports = mongoose.model("User", userSchema);
