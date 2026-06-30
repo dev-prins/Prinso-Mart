18b3f1d4dce656ed6d0d2734572478675bb8768d
Prins Soni <princesoni5010@gmail.com>
2026-06-30 07:59:20 +0530

    Add feature toggle schema to featureToggleController

--- controllers/featureToggleController.js
+++ controllers/featureToggleController.js
@@ -1,1 +1,21 @@
 
+const mongoose = require("mongoose");
+
+const featureToggleSchema = new mongoose.Schema(
+  {
+    key: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    enabled: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("FeatureToggle", featureToggleSchema);
