1099641ec95ef0eddba6c31c721a9bb30dae4833
Prins Soni <princesoni5010@gmail.com>
2026-06-30 08:05:02 +0530

    Add feature toggle controller methods

--- controllers/featureToggleController.js
+++ controllers/featureToggleController.js
@@ -1,6 +1,14 @@
- getFeatureToggle
-updateFeatureToggle
+const FeatureToggle = require("../FeatureToggle");
+
+const getFeatureToggle = async (req, res) => {
+  res.json({ success: true, message: "Feature Toggle Working" });
+};
+
+const updateFeatureToggle = async (req, res) => {
+  res.json({ success: true, message: "Feature Toggle Updated" });
+};
+
 module.exports = {
   getFeatureToggle,
-  updateFeatureToggle
-}
+  updateFeatureToggle,
+};
